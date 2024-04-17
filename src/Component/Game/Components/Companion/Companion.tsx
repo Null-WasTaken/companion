af29a7ced5c93e6b3fab0d0f45940189f796fc28
Amir Hossein Fallah <notrealamir1385@gmail.com>
2024-04-17 07:40:17 -0700

    Dave

--- src/Component/Game/Components/Companion/Companion.tsx
+++ src/Component/Game/Components/Companion/Companion.tsx
@@ -5,11 +5,12 @@ import { PiCodesandboxLogoLight } from "react-icons/pi";
 
 export default function Companion() {
 
-    const [hpBar , Sethpbar] = useState(100);
+    const [hpBar , Sethpbar] = useState(0);
+    console.log(hpBar)
 
     return (
         <div className="flex items-center flex-col w-full">
-        <div className="w-44 h-60 bg-black" id="Dave"></div>
+        <div className={"h-60 bg-black"} style={{width: hpBar,}} id="Dave"></div>
 
                   <div className="border-t-2 w-full pl-2 pr-2 pb-2 pt-2">
                 <span className="text-5xl cursor-pointer" onClick={() => {
